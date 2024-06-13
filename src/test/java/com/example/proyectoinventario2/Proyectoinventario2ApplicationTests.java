e7c0708665a1c4ae1d689aa0c1896f0c99cd60e9
WABTEC\marcos.acosta <marcos.acosta@wabtec.com>
2024-06-13 13:25:59 -0600

    Primer commit

---
+++ src/test/java/com/example/proyectoinventario2/Proyectoinventario2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyectoinventario2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyectoinventario2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
