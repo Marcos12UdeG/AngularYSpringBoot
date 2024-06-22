d03afb2016fdf07623d7830a7c0b5d8d626d6d07
WABTEC\marcos.acosta <marcos.acosta@wabtec.com>
2024-06-22 13:08:15 -0600

    Quinto commit

--- src/main/java/com/example/proyectoinventario2/Controller/CategoryController.java
+++ src/main/java/com/example/proyectoinventario2/Controller/CategoryController.java
@@ -3,11 +3,11 @@ package com.example.proyectoinventario2.Controller;
 import com.example.proyectoinventario2.model.Category;
 import com.example.proyectoinventario2.response.CategoryResponseRest;
 import com.example.proyectoinventario2.services.CategoryServices;
-import com.example.proyectoinventario2.services.CategoryServicesIMPL;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api/v1")
 public class CategoryController {
